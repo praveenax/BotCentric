4e5f389306b64443d0e0324063348f22113a2e81
Praveen <xav.axp@gmail.com>
2017-08-19 12:47:43 +0530

    Added Basic Flask Server

---
+++ TFEngine/TF/Predict.py
@@ -0,0 +1,10 @@
+
+
+
+class Predictor:
+#    def __init__(self):
+#        self.in = "Test"
+        
+
+    def get_tasks(self):
+        print 'task'
---
+++ TFEngine/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import sys
+sys.path.append('TF')
+from Predict import Predictor
+app = Flask(__name__)
+
+predictor = Predictor()
+
+@app.route("/")
+def hello():
+    predictor.get_tasks()
+    return "Hello World!"
+
+
+@app.route("/test")
+def test():
+    return "test"
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=3004)
